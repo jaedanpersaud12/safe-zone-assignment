bd08ee9858ae8f0bced08b02ea99206d3bfdbbbd
jaedanpersaud12 <jaedanpersaud12@gmail.com>
2021-11-09 09:09:50 -0400

    Update for Assignment 9

--- script.js
+++ script.js
@@ -1,35 +1,74 @@
-//Create object for id card with property 'name'
-var idCard = {
-  name: 'Abdool Persaud',
-};
+// Created Object in constructor notation for idCard object and defined some properties and a method to check if the expiry date on the ID Card has passed.
+var idCard = new Object();
+idCard.name = 'Jaedan Persaud',
+  idCard.expiryDate = new Date('Jan 1, 2024, 00:00:00'),
+  idCard.today = new Date(),
+  idCard.checkExpiry = function() {
+    var difference = idCard.today.getTime() - idCard.expiryDate.getTime();
+    var sign = Math.sign(difference)
+    if (sign == -1) {
+      return false;
+    } else if (sign == 1) {
+      return true;
+    }
+  };
 
+//returned value from method and declared it in a variable "expired"
+var expired = idCard.checkExpiry();
 
-/*Create object for vaccine card with properties; 'name' , 'vaccinationType',
-and a boolean 'fullyVaccinated' which states whether the patron has been fully vaccinated or not */
+//created object in literal notation for the vaccination card object and defined some properties and 2 methods;
 var vaccCard = {
   name: 'Jaedan Persaud',
-  vaccinationType: "J&J",
-  fullyVaccinated: true,
-}
+  vaccinationType: 'J&J',
+  secondDose: new Date('Nov 5, 2021 12:00:00'),
+  vaccinated: true,
 
-/*if statement which outputs a message determined by if the name on the vaccine card and the id
-card match and also if the 'fullyVaccinated' boolean is true or false*/
+  //one method to check the difference between 2 dates; the day the patron took his last, and final, vaccination shot and today. to determine if 14 days has elapsed since then.
+  checkDifference: function() {
+    var today = new Date();
+    var difference = today.getTime() - vaccCard.secondDose.getTime();
+    difference = Math.floor(difference / 86400000);
+    return difference;
+  },
 
-//if both statements are true a message is outputted saying the patron can enter the premises
- if (idCard.name == vaccCard.name && vaccCard.fullyVaccinated == true){
-  var result = "Patron is fully vaccinated! Please Enjoy! :)";
+  //the second method is to run through a set of conditions based on given information to determine if a patron meets all criteria to enter the safe zone.
+  checkVaccination: function() {
+    var date = vaccCard.checkDifference();
+
+    var daysUntil = 14 - date;
+
+    if (daysUntil == 1) {
+      var daysMessage = " day";
+    } else {
+      var daysMessage = " days";
+    }
+
+    if (expired == false) {
+      if (idCard.name == vaccCard.name) {
+        if (vaccCard.vaccinated == true) {
+          if (date >= 14) {
+            var message = "You've been fully vaccinated!";
+            return message;
+          } else {
+            var message = "You have " + daysUntil + daysMessage + " until you are allowed entry.";
+            return message;
+          }
+        } else {
+          var message = "You have not been vaccinated. You cannot enter.";
+          return message;
+        }
+      } else {
+        var message = "The name on the ID card does not match the name on the vaccine card. You cannot enter.";
+        return message;
+      }
+    } else {
+      var message = "Your ID Card has expired. You cannot enter.";
+      return message;
+    }
   }
+};
 
-// if either of the statements are false a message is outputted saying the patron cannot enter
+var message = vaccCard.checkVaccination();
 
-else {
-  var result = "Patron has not been vaccinated. Kindly exit.";
-}
-
-//default message
-//var result = "Patron is fully vaccinated! Please Enjoy! :)";
-
-//writes the output to the document object in the browser
-document.write("<h3>Patron Name: <span class=\"bold\">" + idCard.name + "</span></h3>")
-document.write("<h3>Vaccine Type: <span class=\"bold\">" + vaccCard.vaccinationType + " </span></h3>");
-document.write("<h3>" + result + "</h3>");
+var messageEl = document.getElementById("message");
+messageEl.textContent = message;
